248d446a10af2c38c288d01450bd9f91f0675819
alanis <alanifotis@yahoo.com>
2023-12-10 19:44:33 +0100

    Day 2 part 2 DONE

--- SecondAOC.java
+++ SecondAOC.java
@@ -13,50 +13,70 @@ public class SecondAOC {
 
             int gameIds = 1;
             int total = 0;
-            
-            Map<String, Integer> defaultMap = Map.of("blue", 14, "green", 13, "red", 12);            
+            int totalTwo = 0;
+
+            Map<String, Integer> defaultMap = Map.of("blue", 14, "green", 13, "red", 12);
+
             Pattern pattern = Pattern.compile("(\\d+)\\s+(\\w+)");
-                  
+
             List<String> file = Files.readAllLines(Paths.get("input2p1"));
 
             for (String line : file) {
                 boolean wonGame = true;
-                              
+
+                HashMap<String, Integer> partTwo = new HashMap<>();
+
+                partTwo.put("blue", 0);
+                partTwo.put("green", 0);
+                partTwo.put("red", 0);
+
+
+
                 String[] sets = line.split("\\s*;\\s*|\n");
 
 innerloop:for (String set: sets) {
-                    Map<String, Integer> setOne = new HashMap<>();
+              HashMap<String, Integer> setOne = new HashMap<>();
 
-                    setOne.put("blue", 0);
-                    setOne.put("green", 0);
-                    setOne.put("red", 0);
-                    Matcher matcher = pattern.matcher(set);
+              setOne.put("blue", 0);
+              setOne.put("green", 0);
+              setOne.put("red", 0);
+              Matcher matcher = pattern.matcher(set);
 
-                    while (matcher.find() && wonGame) {
+              while (matcher.find()) {
 
-                        int count = Integer.parseInt(matcher.group(1));
-                        String color = matcher.group(2);
+                  int count = Integer.parseInt(matcher.group(1));
+                  String color = matcher.group(2);
+                  if (wonGame) {
+                      setOne.put(color, setOne.getOrDefault(color, 0) + count);
 
-                        setOne.put(color, setOne.getOrDefault(color, 0) + count);
+                      if ((setOne.getOrDefault("red", 0) > defaultMap.getOrDefault("red", 0)) || (setOne.getOrDefault("blue", 0) > defaultMap.getOrDefault("blue", 0)) || (setOne.getOrDefault("green", 0) > defaultMap.getOrDefault("green", 0))) {
+                          wonGame = false;
+                      }
+                  }
 
-                        if ((setOne.getOrDefault("red", 0) > defaultMap.getOrDefault("red", 0)) || (setOne.getOrDefault("blue", 0) > defaultMap.getOrDefault("blue", 0)) || (setOne.getOrDefault("green", 0) > defaultMap.getOrDefault("green", 0))) {
-                            wonGame = false;
-                            break innerloop;
-                        }
-                    }
-                }
 
-                
-                if(wonGame) {
-                    total += gameIds;
-                }
 
-                gameIds++;
+                  if (count >= partTwo.getOrDefault(color, 0)) {
+                      partTwo.put(color , count);
+                  }
+              }
+}
+
+int red = partTwo.getOrDefault("red", 0);
+int green = partTwo.getOrDefault("green", 0);
+int blue = partTwo.getOrDefault("blue", 0);
+
+totalTwo += red * green * blue;
+if(wonGame) {
+    total += gameIds;
+}
+
+gameIds++;
             }
 
-            System.out.println("Total: " + total);
-        } catch (Exception e) {
-            System.out.println(e);
-        }
+System.out.println("Total: " + total + "\nTotal Two: " + totalTwo);
+} catch (Exception e) {
+    System.out.println(e);
+}
     }
 }
